f4e1d1c8cbe134cdd06471bf7f84f65647b39b98
drizh2 <drizh2@ukr.net>
2023-07-21 12:37:08 +0300

    Added classes for User, Post, Comment and ImageModel. Added annotations for database using. Added enum ERoles(roles definition).

---
+++ src/main/java/com/drizh2/instazoo/entities/User.java
@@ -0,0 +1,53 @@
+package com.drizh2.instazoo.entities;
+
+import com.drizh2.instazoo.entities.enums.ERoles;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String lastname;
+    @Column(unique = true, updatable = false)
+    private String username;
+    @Column(unique = true)
+    private String email;
+    @Column(columnDefinition = "text")
+    private String bio;
+    @Column(length = 3000)
+    private String password;
+
+    @ElementCollection(targetClass = ERoles.class)
+    @CollectionTable(name = "user_role",
+    joinColumns = @JoinColumn(name = "user_id"))
+    private Set<ERoles> roles = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public User() {
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/drizh2/instazoo/entities/enums/ERoles.java
@@ -0,0 +1,6 @@
+package com.drizh2.instazoo.entities.enums;
+
+public enum ERoles {
+    ROLE_USER,
+    ROLE_ADMIN
+}
