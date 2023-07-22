73bf3fc19f640350ee00cd216c8ee78665c5c0bf
drizh2 <drizh2@ukr.net>
2023-07-22 18:04:42 +0300

    Added missed annotations.

--- src/main/java/com/drizh2/instazoo/repositories/CommentRepository.java
+++ src/main/java/com/drizh2/instazoo/repositories/CommentRepository.java
@@ -3,9 +3,11 @@ package com.drizh2.instazoo.repositories;
 import com.drizh2.instazoo.entities.Comment;
 import com.drizh2.instazoo.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByPost(Post post);
 
